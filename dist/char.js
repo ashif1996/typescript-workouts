5946aba65805565e4bb00337af0bfb896f0bc381
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-20 01:17:22 +0530

    add compiled javascript code

---
+++ dist/char.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline_sync_1 = __importDefault(require("readline-sync"));
+var char = readline_sync_1.default.question("\nEnter your name: ");
+console.log("\n" + "Welcome", char + "\n");
