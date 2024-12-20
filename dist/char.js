fdf682ebb4e72d72aaaf7912d2077bbb81faa7a0
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-20 12:23:28 +0530

    add compiled JavaScript file for sum calculation

--- dist/char.js
+++ dist/char.js
@@ -4,5 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var readline_sync_1 = __importDefault(require("readline-sync"));
-var char = readline_sync_1.default.question("\n" + "Enter your name: ");
-console.log("\n" + "Welcome", char + "\n");
+var displayCharInput = function () {
+    var char = readline_sync_1.default.question("\n" + "Enter your name: ");
+    return char;
+};
+var name = displayCharInput();
+console.log("\n" + "Welcome ".concat(name) + "\n");
---
+++ dist/sum.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline_sync_1 = __importDefault(require("readline-sync"));
+var displaySum = function () {
+    var num1 = parseFloat(readline_sync_1.default.question("\n" + "Enter the first number: "));
+    var num2 = parseFloat(readline_sync_1.default.question("Enter the first number: "));
+    var sum = num1 + num2;
+    return sum;
+};
+var result = displaySum();
+console.log("\n" + "The sum is ".concat(result, ".") + "\n");
