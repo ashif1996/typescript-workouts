f6b6bb602ff38ed79968f2ad5c48e57390079776
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-20 19:57:20 +0530

    add TypeScript implementation for printing multiplication table

---
+++ workouts/multiplication.ts
@@ -0,0 +1,17 @@
+// Write a program to print the multiplication table of given numbers.
+
+import readlineSync from "readline-sync";
+
+const printMultiplicationTable = (): void => {
+    const num: number = parseFloat(readlineSync.question("\n"+ "Enter a number: "));
+    console.log("\n");
+    
+    for (let i: number = 1; i <= 10; i++) {
+        const multiplication: number = i * num;
+        console.log(`${i} x ${num} = ${multiplication}`);
+    }
+
+    console.log("\n");
+};
+
+printMultiplicationTable();
